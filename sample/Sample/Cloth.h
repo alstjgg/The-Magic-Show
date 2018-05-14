6ab63f8753a8d8054b8898f778d92a58260cdfdd
서강욱 <tjrkddnr@korea.ac.kr>
2018-05-14 09:07:32 +0900

    VECTOR3D 적용
    Vec3 class를 VECTOR3D class로 대체함

--- sample/Sample/Cloth.h
+++ sample/Sample/Cloth.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <vector>
 #include "Constraint.h"
-#include <gl.h>
-#include <glut.h> 
+#include <gl/GL.h>
+#include <gl/glut.h> 
 
 class Cloth
 {
@@ -23,42 +23,42 @@ private:
 	normal vector of the triangle defined by the position of the particles p1, p2, and p3.
 	The magnitude of the normal vector is equal to the area of the parallelogram defined by p1, p2 and p3
 	*/
-	Vec3 calcTriangleNormal(Particle *p1, Particle *p2, Particle *p3)
+	VECTOR3D calcTriangleNormal(Particle *p1, Particle *p2, Particle *p3)
 	{
-		Vec3 pos1 = p1->getPos();
-		Vec3 pos2 = p2->getPos();
-		Vec3 pos3 = p3->getPos();
+		VECTOR3D pos1 = p1->getPos();
+		VECTOR3D pos2 = p2->getPos();
+		VECTOR3D pos3 = p3->getPos();
 
-		Vec3 v1 = pos2 - pos1;
-		Vec3 v2 = pos3 - pos1;
+		VECTOR3D v1 = pos2 - pos1;
+		VECTOR3D v2 = pos3 - pos1;
 
-		return v1.cross(v2);
+		return v1.CrossProduct(v2);
 	}
 
 	/* A private method used by windForce() to calcualte the wind force for a single triangle
 	defined by p1,p2,p3*/
-	void addWindForcesForTriangle(Particle *p1, Particle *p2, Particle *p3, const Vec3 direction)
+	void addWindForcesForTriangle(Particle *p1, Particle *p2, Particle *p3, const VECTOR3D direction)
 	{
-		Vec3 normal = calcTriangleNormal(p1, p2, p3);
-		Vec3 d = normal.normalized();
-		Vec3 force = normal*(d.dot(direction));
+		VECTOR3D normal = calcTriangleNormal(p1, p2, p3);
+		VECTOR3D d = normal.Normalize();
+		VECTOR3D force = normal*(d.InnerProduct(direction));
 		p1->addForce(force);
 		p2->addForce(force);
 		p3->addForce(force);
 	}
 
 	/* A private method used by drawShaded(), that draws a single triangle p1,p2,p3 with a color*/
-	void drawTriangle(Particle *p1, Particle *p2, Particle *p3, const Vec3 color)
+	void drawTriangle(Particle *p1, Particle *p2, Particle *p3, const VECTOR3D color)
 	{
 		glColor3fv((GLfloat*)&color);
 
-		glNormal3fv((GLfloat *) &(p1->getNormal().normalized()));
+		glNormal3fv((GLfloat *) &(p1->getNormal().Normalize()));
 		glVertex3fv((GLfloat *) &(p1->getPos()));
 
-		glNormal3fv((GLfloat *) &(p2->getNormal().normalized()));
+		glNormal3fv((GLfloat *) &(p2->getNormal().Normalize()));
 		glVertex3fv((GLfloat *) &(p2->getPos()));
 
-		glNormal3fv((GLfloat *) &(p3->getNormal().normalized()));
+		glNormal3fv((GLfloat *) &(p3->getNormal().Normalize()));
 		glVertex3fv((GLfloat *) &(p3->getPos()));
 	}
 
@@ -74,9 +74,10 @@ public:
 		{
 			for (int y = 0; y<num_particles_height; y++)
 			{
-				Vec3 pos = Vec3(width * (x / (float)num_particles_width),
+				VECTOR3D pos = VECTOR3D(width * (x / (float)num_particles_width),
 					-height * (y / (float)num_particles_height),
-					0);
+				0);
+
 				particles[y*num_particles_width + x] = Particle(pos); // insert particle in column x at y'th row
 			}
 		}
@@ -110,10 +111,10 @@ public:
 		// making the upper left most three and right most three particles unmovable
 		for (int i = 0; i<3; i++)
 		{
-			getParticle(0 + i, 0)->offsetPos(Vec3(0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
+			getParticle(0 + i, 0)->offsetPos(VECTOR3D(0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
 			getParticle(0 + i, 0)->makeUnmovable();
 
-			getParticle(num_particles_width - 1 - i, 0)->offsetPos(Vec3(-0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
+			getParticle(num_particles_width - 1 - i, 0)->offsetPos(VECTOR3D(-0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
 			getParticle(num_particles_width - 1 - i, 0)->makeUnmovable();
 		}
 	}
@@ -142,7 +143,7 @@ public:
 		{
 			for (int y = 0; y<num_particles_height - 1; y++)
 			{
-				Vec3 normal = calcTriangleNormal(getParticle(x + 1, y), getParticle(x, y), getParticle(x, y + 1));
+				VECTOR3D normal = calcTriangleNormal(getParticle(x + 1, y), getParticle(x, y), getParticle(x, y + 1));
 				getParticle(x + 1, y)->addToNormal(normal);
 				getParticle(x, y)->addToNormal(normal);
 				getParticle(x, y + 1)->addToNormal(normal);
@@ -159,11 +160,11 @@ public:
 		{
 			for (int y = 0; y<num_particles_height - 1; y++)
 			{
-				Vec3 color(0, 0, 0);
+				VECTOR3D color(0, 0, 0);
 				if (x % 2) // red and white color is interleaved according to which column number
-					color = Vec3(0.6f, 0.2f, 0.2f);
+					color = VECTOR3D(0.6f, 0.2f, 0.2f);
 				else
-					color = Vec3(1.0f, 1.0f, 1.0f);
+					color = VECTOR3D(1.0f, 1.0f, 1.0f);
 
 				drawTriangle(getParticle(x + 1, y), getParticle(x, y), getParticle(x, y + 1), color);
 				drawTriangle(getParticle(x + 1, y + 1), getParticle(x + 1, y), getParticle(x, y + 1), color);
@@ -194,7 +195,7 @@ public:
 	}
 
 	/* used to add gravity (or any other arbitrary vector) to all particles*/
-	void addForce(const Vec3 direction)
+	void addForce(const VECTOR3D direction)
 	{
 		std::vector<Particle>::iterator particle;
 		for(int i=0;i<particles.size();i++)
@@ -205,7 +206,7 @@ public:
 	}
 
 	/* used to add wind forces to all particles, is added for each triangle since the final force is proportional to the triangle area as seen from the wind direction*/
-	void windForce(const Vec3 direction)
+	void windForce(const VECTOR3D direction)
 	{
 		for (int x = 0; x<num_particles_width - 1; x++)
 		{
@@ -221,16 +222,16 @@ public:
 	This is based on a very simples scheme where the position of each particle is simply compared to the sphere and corrected.
 	This also means that the sphere can "slip through" if the ball is small enough compared to the distance in the grid bewteen particles
 	*/
-	void ballCollision(const Vec3 center, const float radius)
+	void ballCollision(const VECTOR3D center, const float radius)
 	{
 		std::vector<Particle>::iterator particle;
 		for(int i=0;i<particles.size();i++)
 		{
-			Vec3 v = particles[i].getPos() - center;
-			float l = v.length();
-			if (v.length() < radius) // if the particle is inside the ball
+			VECTOR3D v = particles[i].getPos() - center;
+			float l = v.Magnitude();
+			if (v.Magnitude() < radius) // if the particle is inside the ball
 			{
-				particles[i].offsetPos(v.normalized()*(radius - l)); // project the particle to the surface of the ball
+				particles[i].offsetPos(v.Normalize()*(radius - l)); // project the particle to the surface of the ball
 			}
 		}
 	}
--- sample/Sample/Particle.h
+++ sample/Sample/Particle.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Vec3.h"
+#include "VECTOR3D.h"
 
 /* Some physics constants */
 #define DAMPING 0.01 // how much to damp the cloth simulation each frame
@@ -14,16 +14,16 @@ private:
 	bool movable; // can the particle move or not ? used to pin parts of the cloth
 
 	float mass; // the mass of the particle (is always 1 in this example)
-	Vec3 pos; // the current position of the particle in 3D space
-	Vec3 old_pos; // the position of the particle in the previous time step, used as part of the verlet numerical integration scheme
-	Vec3 acceleration; // a vector representing the current acceleration of the particle
-	Vec3 accumulated_normal; // an accumulated normal (i.e. non normalized), used for OpenGL soft shading
+	VECTOR3D pos; // the current position of the particle in 3D space
+	VECTOR3D old_pos; // the position of the particle in the previous time step, used as part of the verlet numerical integration scheme
+	VECTOR3D acceleration; // a vector representing the current acceleration of the particle
+	VECTOR3D accumulated_normal; // an accumulated normal (i.e. non normalized), used for OpenGL soft shading
 
 public:
-	Particle(Vec3 pos) : pos(pos), old_pos(pos), acceleration(Vec3(0, 0, 0)), mass(1), movable(true), accumulated_normal(Vec3(0, 0, 0)) {}
+	Particle(VECTOR3D pos) : pos(pos), old_pos(pos), acceleration(VECTOR3D(0, 0, 0)), mass(1), movable(true), accumulated_normal(VECTOR3D(0, 0, 0)) {}
 	Particle() {}
 
-	void addForce(Vec3 f)
+	void addForce(VECTOR3D f)
 	{
 		acceleration += f / mass;
 	}
@@ -35,28 +35,28 @@ public:
 	{
 		if (movable)
 		{
-			Vec3 temp = pos;
+			VECTOR3D temp = pos;
 			pos = pos + (pos - old_pos)*(1.0 - DAMPING) + acceleration*TIME_STEPSIZE2;
 			old_pos = temp;
-			acceleration = Vec3(0, 0, 0); // acceleration is reset since it HAS been translated into a change in position (and implicitely into velocity)	
+			acceleration = VECTOR3D(0, 0, 0); // acceleration is reset since it HAS been translated into a change in position (and implicitely into velocity)	
 		}
 	}
 
-	Vec3& getPos() { return pos; }
+	VECTOR3D& getPos() { return pos; }
 
-	void resetAcceleration() { acceleration = Vec3(0, 0, 0); }
+	void resetAcceleration() { acceleration = VECTOR3D(0, 0, 0); }
 
-	void offsetPos(const Vec3 v) { if (movable) pos += v; }
+	void offsetPos(const VECTOR3D v) { if (movable) pos += v; }
 
 	void makeUnmovable() { movable = false; }
 
-	void addToNormal(Vec3 normal)
+	void addToNormal(VECTOR3D normal)
 	{
-		accumulated_normal += normal.normalized();
+		accumulated_normal += normal.Normalize();
 	}
 
-	Vec3& getNormal() { return accumulated_normal; } // notice, the normal is not unit length
+	VECTOR3D& getNormal() { return accumulated_normal; } // notice, the normal is not unit length
 
-	void resetNormal() { accumulated_normal = Vec3(0, 0, 0); }
+	void resetNormal() { accumulated_normal = VECTOR3D(0, 0, 0); }
 
 };
